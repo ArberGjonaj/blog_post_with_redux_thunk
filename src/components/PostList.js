8a6f4e871c05a9d984f0b97fabe61be9eed49488
Arber Gjonaj <gjonajarber@yahoo.com>
2020-10-20 10:34:32 +0200

    Using memoize

---
+++ src/components/PostList.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
+class PostList extends React.Component {
+
+   componentDidMount(){
+       this.props.fetchPosts();
+     
+   
+    }
+    renderList(){
+    return this.props.posts.map(posts=>{
+      return (
+          <div  className={"item"} key={posts.id}>
+            <i className={"large middle aligned icon user"}/>
+            <div className={"content"}>
+              <div className={"description"}>
+               <h2>{posts.title}</h2>
+               <p>{posts.body}</p>
+               <UserHeader userId={posts.userId}/>
+              </div>
+            </div>
+          </div>
+      )
+    })
+    }
+
+  render() {
+console.log(this.props.posts)
+    return (
+
+    <div className={"ui relaxed divided list"}>
+        {this.renderList()}
+    </div>
+      
+    )
+  }
+}
+
+const mapStateToProps=(state)=>{
+  return {posts:state.posts} ;
+  }
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
